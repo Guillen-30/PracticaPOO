fc4cfc4edf3898b741b55160ac9205104161fd54
Guillen-30 <sguillenguzman@gmail.com>
2023-08-18 15:59:24 -0600

    Agregar Cambios Obstaculos

--- cargame/src/track/Obstaculo.java
+++ cargame/src/track/Obstaculo.java
@@ -1,5 +1,14 @@
 package track;
 
 public class Obstaculo {
-
+	private String tipo;
+	private int damage;
+	
+	public int getDamage() {
+		return this.damage;
+	}
+	
+	public String getTipo() {
+		return this.tipo;
+	}
 }
